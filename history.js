55d8aaa10cde358155f883abf7a9f924c24bd7ad
shantosarker2001 <sarkershant0303@gmail.com>
2022-09-15 01:18:11 +0530

    complete-blog

--- history.js
+++ history.js
@@ -3,7 +3,7 @@ const histories = document.getElementById("histories");
 function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
-
+  newRow.classList.add("text-black")
   newRow.innerHTML = `
   <h3>${questionText}</h3>
   <div>
